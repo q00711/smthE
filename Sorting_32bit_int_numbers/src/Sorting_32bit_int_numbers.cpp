7177627800cb4a205144d041e6a683eac071dbdf
q00711 <q00711@mail.ru>
2015-03-28 22:09:36 +0400

    init commit

---
+++ Sorting_32bit_int_numbers/src/Sorting_32bit_int_numbers.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : Sorting_32bit_int_numbers.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	for (int i=0; i<50; i++) {
+		cout << rand()%1000000 << endl; // 0 - 999999
+	}
+	return 0;
+}
